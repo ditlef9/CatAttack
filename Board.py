b49f36f08cb0fbef13acd440213441b59923ab32
trn <trn@posteo.de>
2018-12-01 17:45:54 +0100

    Minor refactoring

--- Board.py
+++ Board.py
@@ -1,7 +1,20 @@
 class Board:
-    
 
     def __init__(self, screenSizeX, screenSizeY):
         self.sizeX = int(4)
         self.sizeY = int(3)
-        
+
+# Positions, X, Y
+def getPositionInPixels(pos):
+    position = {
+        "a1": (110, 110),
+        "b1": (110, 260),
+        "c1": (110, 410),
+        "a2": (260, 210),
+        "b2": (260, 260),
+        "c2": (260, 410),
+        "a3": (410, 210),
+        "b3": (410, 260),
+        "c3": (410, 410)
+    }
+    return position.get(pos)
--- Piece.py
+++ Piece.py
@@ -1,85 +1,38 @@
+import Board
+
+
 class Piece:
+    
+    CAT = "cat"
+    DOG = "dog"
+    MONKEY = "monkey"
+    SHEEP = "sheep"
     
     def __init__(self, color, name, position):
         self.color = color
         self.name = name
         self.position = position
-        self.isActive = ""
+        self.isActive = False
         self.imagePiece = ""
+        
     
     
     def addPieceToBoard(self, pygame, screen):
+        
+        def getImagePath(animal, color, active=False):
+            filename = "animals/" + animal + "_" + color + "_128"
+            if (active):
+                filename += "_active.png"
+            else:
+                filename += ".png"
+            return filename
+
+                
         # Image
-        if(self.color == "blue" and self.name == "monkey"):
-            if(self.isActive == "true"):
-                self.imagePiece = pygame.image.load("animals/monkey_blue_128_active.png")
-            else:
-                self.imagePiece = pygame.image.load("animals/monkey_blue_128.png")
-        elif(self.color == "blue" and self.name == "cat"):
-            if(self.isActive == "true"):
-                self.imagePiece = pygame.image.load("animals/cat_blue_128_active.png")
-            else:
-                self.imagePiece = pygame.image.load("animals/cat_blue_128.png")
-        elif(self.color == "blue" and self.name == "dog"):
-            if(self.isActive == "true"):
-                self.imagePiece = pygame.image.load("animals/dog_blue_128_active.png")
-            else:
-                self.imagePiece = pygame.image.load("animals/dog_blue_128.png")
-        elif(self.color == "blue" and self.name == "sheep"):
-            if(self.isActive == "true"):
-                self.imagePiece = pygame.image.load("animals/sheep_blue_128_active.png")
-            else:
-                self.imagePiece = pygame.image.load("animals/sheep_blue_128.png")
-            
-        if(self.color == "red" and self.name == "monkey"):
-            if(self.isActive == "true"):
-                self.imagePiece = pygame.image.load("animals/monkey_red_128_active.png")
-            else:
-                self.imagePiece = pygame.image.load("animals/monkey_red_128.png")
-        elif(self.color == "red" and self.name == "cat"):
-            if(self.isActive == "true"):
-                self.imagePiece = pygame.image.load("animals/cat_red_128_active.png")
-            else:
-                self.imagePiece = pygame.image.load("animals/cat_red_128.png")
-        elif(self.color == "red" and self.name == "dog"):
-            if(self.isActive == "true"):
-                self.imagePiece = pygame.image.load("animals/dog_red_128_active.png")
-            else:
-                self.imagePiece = pygame.image.load("animals/dog_red_128.png")
-        elif(self.color == "red" and self.name == "sheep"):
-            if(self.isActive == "true"):
-                self.imagePiece = pygame.image.load("animals/sheep_red_128_active.png")
-            else:
-                self.imagePiece = pygame.image.load("animals/sheep_red_128.png")
+        self.imagePiece = pygame.image.load(getImagePath(self.name, self.color, self.isActive))
 
         # Positions, X, Y
-        if(self.position == "a1"):
-            positionInPixels = (110,110) #list
-        elif(self.position == "b1"):
-            positionInPixels = (110,260) #list
-        elif(self.position == "c1"):
-            positionInPixels = (110,410) #list
-            
-        elif(self.position == "a2"):
-            positionInPixels = (260,210) #list
-        elif(self.position == "b2"):
-            positionInPixels = (260,260) #list
-        elif(self.position == "c2"):
-            positionInPixels = (260,410) #list
-            
-        elif(self.position == "a3"):
-            positionInPixels = (410,210) #list
-        elif(self.position == "b3"):
-            positionInPixels = (410,260) #list
-        elif(self.position == "c3"):
-            positionInPixels = (410,410) #list
-            
-        elif(self.position == "a4"):
-            positionInPixels = (560,110) #list
-        elif(self.position == "b4"):
-            positionInPixels = (560,260) #list
-        elif(self.position == "c4"):
-            positionInPixels = (560,410) #list
+        positionInPixels = Board.getPositionInPixels(self.position) #list
         
         
         
