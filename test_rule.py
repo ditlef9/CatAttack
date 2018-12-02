0a97e8c085df80238f2d25a885d1ab33e0b0ec04
trn <trn@posteo.de>
2018-12-02 02:05:28 +0100

    basic rules monkey & lion

--- test_rule.py
+++ test_rule.py
@@ -6,16 +6,16 @@ import Rule
 class TestRule(TestCase):
     def test_isLegalDogMove(self):
         # Legal step "blue dog move one step forward from default pos"
-        self.assertTrue(self, Rule.isLegalDogMove([0, 2], [0, 3]))
+        self.assertTrue(self, Rule.is_legal_move_dog([0, 2], [0, 3]))
 
         # Legal step "blue dog move one step left from default pos"
-        self.assertTrue(self, Rule.isLegalDogMove([0, 2], [1, 2]))
+        self.assertTrue(self, Rule.is_legal_move_dog([0, 2], [1, 2]))
 
         # Illegal step: "blue dog move two step left from default pos"
-        self.assertTrue(self, Rule.isLegalDogMove([0, 2], [2, 2]) == False)
+        self.assertTrue(self, Rule.is_legal_move_dog([0, 2], [2, 2]) == False)
 
         # Illegal step: "blue dog move two step forward from default pos"
-        self.assertTrue(self, Rule.isLegalDogMove([0, 2], [0, 4]) == False)
+        self.assertTrue(self, Rule.is_legal_move_dog([0, 2], [0, 4]) == False)
 
         # Illegal step: "blue dog move one step diagonally forward left from default pos"
-        self.assertTrue(self, Rule.isLegalDogMove([0, 2], [1, 3]) == False)
+        self.assertTrue(self, Rule.is_legal_move_dog([0, 2], [1, 3]) == False)
